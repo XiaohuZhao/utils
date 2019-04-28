93a7db644931608dede01b997468bf15d41d9f53
zhangjie@yorma.net <zhangjie@yorma.net>
2019-04-28 14:38:07 +0800

    LoggerInterceptor: 简化日志输出 执行结果 StringUtils，CopyUtils: 从 DEC 抽取移至 common-utils

--- src/main/java/com/yorma/common/utils/logger/interceptor/LoggerInterceptor.java
+++ src/main/java/com/yorma/common/utils/logger/interceptor/LoggerInterceptor.java
@@ -11,9 +11,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 拦截方法,判断是否有@SysLogger注解并采取动作
@@ -26,105 +34,249 @@ import java.util.Date;
 @Aspect
 @Component
 public class LoggerInterceptor {
-    public static final String LOG_FORMAT = "[INFO][{}]->方法{}: {}";
-    public static final String BEGIN = "开始执行";
-    public static final String END = "执行结束";
-    public static final String PARAMS = "参数列表";
-    public static final String RESULT = "执行结果";
-    public static final String TIME = "执行用时";
-    public static final String CURRENT_THREAD = "当前线程";
-    public static Logger LOGGER;
+	public static final String LOG_FORMAT = "[INFO][{}]->方法{}: {}";
+	public static final String BEGIN = "开始执行";
+	public static final String END = "执行结束";
+	public static final String PARAMS = "参数列表";
+	public static final String RESULT = "执行结果";
+	public static final String TIME = "执行用时";
+	public static final String CURRENT_THREAD = "当前线程";
+	public static Logger LOGGER;
 
-    private static String toJsonString(Object object) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        String result = null;
-        try {
-            result = objectMapper.writeValueAsString(object);
-        } catch (JsonProcessingException e) {
-            LOGGER.error(e.getMessage());
-        }
-        return result;
-    }
+	private static String toJsonString(Object object) {
+		ObjectMapper objectMapper = new ObjectMapper();
+		String result = null;
+		try {
+			StringBuffer x = new StringBuffer("");
+			Object l = simplifyResult(object, x);
+			if (l != null) {
+				result = objectMapper.writeValueAsString(l).replace("}]", "}......first 10 of " + x + " ]");
 
-    public static String formatDate() {
-        return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
-    }
+			} else
+				result = objectMapper.writeValueAsString(object);
+		} catch (JsonProcessingException e) {
+			LOGGER.error(e.getMessage());
+		}
+		return result;
+	}
 
-    /**
-     * 在控制台输出方法的返回结果
-     *
-     * @param logger 日志记录对象
-     * @param print  是否打印
-     * @param result 方法的返回结果
-     */
-    public static void printResult(Logger logger, boolean print, Object result) {
-        if (print) {
-            logger.info(LOG_FORMAT, formatDate(), RESULT, toJsonString(result));
-        }
-    }
+	public static String formatDate() {
+		return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+	}
 
-    /**
-     * 在控制台输出方法参数
-     *
-     * @param point  切点
-     * @param logger 日志记录对象
-     * @param print  是否打印
-     */
-    public static void printParams(ProceedingJoinPoint point, Logger logger, boolean print) {
-        if (print) {
-            Object[] args = point.getArgs();
-            if (args.length == 0) {
-                return;
-            }
-            logger.info(LOG_FORMAT, formatDate(), PARAMS, toJsonString(args));
-        }
-    }
+	/**
+	 * 在控制台输出方法的返回结果
+	 *
+	 * @param logger
+	 *            日志记录对象
+	 * @param print
+	 *            是否打印
+	 * @param result
+	 *            方法的返回结果
+	 */
+	public static void printResult(Logger logger, boolean print, Object result) {
+		if (print) {
+			logger.warn(LOG_FORMAT, formatDate(), RESULT, toJsonString(result));
+		}
+	}
 
-    /**
-     * 如果有@SysLogger注解采取的动作
-     *
-     * @param point 切点
-     * @return 方法返回的结果
-     */
-    @Around(value = "@within(com.yorma.common.utils.logger.annotation.SysLogger) || @annotation(com.yorma.common.utils.logger.annotation.SysLogger)")
-    public Object logger(ProceedingJoinPoint point) throws Throwable {
-        final long begin = System.currentTimeMillis();
-        Class targetClass = point.getTarget().getClass();
-        LOGGER = LoggerFactory.getLogger(targetClass);
-        Signature signature = point.getSignature();
-        boolean print = isPrint(targetClass, signature.toLongString());
-        System.out.println();
-        LOGGER.info(LOG_FORMAT, formatDate(), CURRENT_THREAD, Thread.currentThread().getName());
-        LOGGER.info(LOG_FORMAT, formatDate(), BEGIN, signature);
-        printParams(point, LOGGER, print);
-        Object result;
-        try {
-            result = point.proceed();
-        } catch (Throwable throwable) {
-            throwable.printStackTrace();
-            LOGGER.error(throwable.toString());
-            throw throwable;
-        }
-        LOGGER.info(LOG_FORMAT, formatDate(), END, signature);
-        printResult(LOGGER, print, result);
-        final long end = System.currentTimeMillis();
-        LOGGER.info(LOG_FORMAT, formatDate(), TIME, (end - begin) + "ms");
-        System.out.println();
-        return result;
-    }
+	/**
+	 * 在控制台输出方法参数
+	 *
+	 * @param point
+	 *            切点
+	 * @param logger
+	 *            日志记录对象
+	 * @param print
+	 *            是否打印
+	 */
+	public static void printParams(ProceedingJoinPoint point, Logger logger, boolean print) {
+		if (print) {
+			Object[] args = point.getArgs();
+			if (args.length == 0) {
+				return;
+			}
+			logger.info(LOG_FORMAT, formatDate(), PARAMS, toJsonString(args));
+		}
+	}
 
-    /**
-     * 返回注解的print值
-     *
-     * @param targetClass         SysLogger标注的目标类或方法所在的类
-     * @param longMethodSignature 方法声明
-     * @return 是否打印
-     */
-    public boolean isPrint(Class targetClass, String longMethodSignature) {
-        return targetClass.isAnnotationPresent(SysLogger.class)
-                && ((SysLogger) targetClass.getDeclaredAnnotation(SysLogger.class)).print()
-                || Arrays.stream(targetClass.getDeclaredMethods())
-                .filter(method -> method.toString().equals(longMethodSignature) && method.isAnnotationPresent(SysLogger.class))
-                .anyMatch(method -> method.getAnnotation(SysLogger.class).print());
-    }
+	/**
+	 * 如果有@SysLogger注解采取的动作
+	 *
+	 * @param point
+	 *            切点
+	 * @return 方法返回的结果
+	 */
+	@Around(value = "@within(com.yorma.common.utils.logger.annotation.SysLogger) || @annotation(com.yorma.common.utils.logger.annotation.SysLogger)")
+	public Object logger(ProceedingJoinPoint point) throws Throwable {
+		final long begin = System.currentTimeMillis();
+		Class targetClass = point.getTarget().getClass();
+		LOGGER = LoggerFactory.getLogger(targetClass);
+		Signature signature = point.getSignature();
+		boolean print = isPrint(targetClass, signature.toLongString());
+		System.out.println();
+		LOGGER.info(LOG_FORMAT, formatDate(), CURRENT_THREAD, Thread.currentThread().getName());
+		LOGGER.info(LOG_FORMAT, formatDate(), BEGIN, signature);
+		printParams(point, LOGGER, print);
+		Object result;
+		try {
+			result = point.proceed();
+		} catch (Throwable throwable) {
+			throwable.printStackTrace();
+			LOGGER.error(throwable.toString());
+			throw throwable;
+		}
+		LOGGER.info(LOG_FORMAT, formatDate(), END, signature);
+		printResult(LOGGER, print, result);
+		final long end = System.currentTimeMillis();
+		LOGGER.info(LOG_FORMAT, formatDate(), TIME, (end - begin) + "ms");
+		System.out.println();
+		return result;
+	}
+
+	/**
+	 * 返回注解的print值
+	 *
+	 * @param targetClass
+	 *            SysLogger标注的目标类或方法所在的类
+	 * @param longMethodSignature
+	 *            方法声明
+	 * @return 是否打印
+	 */
+	public boolean isPrint(Class targetClass, String longMethodSignature) {
+		return targetClass.isAnnotationPresent(SysLogger.class)
+				&& ((SysLogger) targetClass.getDeclaredAnnotation(SysLogger.class)).print()
+				|| Arrays.stream(targetClass.getDeclaredMethods())
+						.filter(method -> method.toString().equals(longMethodSignature)
+								&& method.isAnnotationPresent(SysLogger.class))
+						.anyMatch(method -> method.getAnnotation(SysLogger.class).print());
+	}
+
+	/**
+	 * 简化 List<T> 日志输出信息
+	 * 
+	 * @param object
+	 * @return
+	 */
+	private static Object simplifyResult(Object object, StringBuffer x) {
+
+		String C_RS_MSG = "com.yorma.common.entity.dto.ResponseMessage";
+		String C_RS_DATA = "com.yorma.common.entity.dto.ResponseData";
+
+		System.err.println("Classssss: " + object.getClass().getName());
+
+		Object r = object;
+		Object d = null;
+		List<?> l = null;
+		Map<?, ?> map = null;
+
+		try {
+			Class cr = Class.forName(C_RS_MSG);
+			Class cd = Class.forName(C_RS_DATA);
+
+			Constructor cnt_r = cr.getDeclaredConstructor(Boolean.class, String.class, String.class);
+			Method mr = cr.getDeclaredMethod("getData");
+			Method mr0 = cr.getDeclaredMethod("getSuccess");
+			Method mr1 = cr.getDeclaredMethod("getStatus");
+			Method mr2 = cr.getDeclaredMethod("getMsg");
+			Method mr3 = cr.getDeclaredMethod("setData", Object.class);
+
+//			Constructor cnt_d = cd.getDeclaredConstructor(Integer.class, Integer.class, Integer.class, Integer.class,
+			Constructor cnt_d = cd.getDeclaredConstructor(int.class, int.class, int.class, Integer.TYPE, List.class);
+			Method ml = cd.getDeclaredMethod("getList");
+			Method ml0 = cd.getDeclaredMethod("getPageSize");
+			Method ml1 = cd.getDeclaredMethod("getPageNumber");
+			Method ml2 = cd.getDeclaredMethod("getTotalPage");
+			Method ml3 = cd.getDeclaredMethod("getTotalRow");
+
+			if (r.getClass().equals(cr)) {
+				d = mr.invoke(r);
+
+				if(d==null)
+					return null;
+				if (cd.equals(d.getClass())) {
+					l = (List) ml.invoke(d);
+
+				} else if (d instanceof List) {
+					l = (List<?>) d;
+					d = null;
+				}
+
+			}else if(r instanceof List) {
+//				System.out.println("List..........XXXXXX: " + r.getClass());
+				l = (List)r;
+				r = null;
+				d = null;
+
+			}else if(r instanceof Map) {
+//				System.out.println("Map..........MMMMMM: " + r.getClass());
+				map = (Map)r;
+				r = null;
+				d = null;
+			}else {
+				System.out.println("Others..........: " + r.getClass());
+				r = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+			}
+
+			if(map != null) {
+				x.append(map.size());
+				HashMap m = new HashMap();
+				int i = 0;
+				for(Object key : map.keySet()) {
+					m.put(key, map.get(key));
+					if(++i < 10)
+						break;
+				}
+				
+				r = m;
+
+			}else
+			if (l != null && l.size() > 50) {
+				x.append(l.size());
+
+				List li = new ArrayList();
+				for (int i = 0; i < 10; i++) {
+					li.add(l.get(i));
+				}
+				l = li;
+
+				if(r == null) {
+					r = l;
+				}else if (d == null) {
+					r = cnt_r.newInstance(mr0.invoke(r), mr1.invoke(r), mr2.invoke(r));
+					mr3.invoke(r, l);
+
+				} else {
+					d = cnt_d.newInstance(ml0.invoke(d), ml1.invoke(d), ml2.invoke(d), ml3.invoke(d), l);
+					r = cnt_r.newInstance(mr0.invoke(r), mr1.invoke(r), mr2.invoke(r));
+					mr3.invoke(r, d);
+
+				}
+			}
+
+		} catch (ClassNotFoundException e) {
+
+		} catch (NoSuchMethodException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return r;
+	}
+
 }
---
+++ src/main/java/com/yorma/common/utils/object/CopyUtils.java
@@ -0,0 +1,234 @@
+package com.yorma.common.utils.object;
+
+import java.lang.reflect.Constructor;    
+import java.lang.reflect.Field;    
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.Arrays;    
+import java.util.Collection;    
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;    
+import java.util.Set; 
+
+  
+import java.beans.BeanInfo;  
+import java.beans.Introspector;  
+import java.beans.PropertyDescriptor;  
+/**
+ * @author 窦琪
+ * @2018年1月10日
+ */
+public class CopyUtils {
+	   
+    /**  
+     * 无需进行复制的特殊类型数组  
+     */    
+    static Class[] needlessCloneClasses = new Class[]{String.class,Boolean.class,Character.class,Byte.class,Short.class,    
+        Integer.class,Long.class,Float.class,Double.class,Void.class,Object.class,Class.class    
+    };    
+    /**  
+     * 判断该类型对象是否无需复制  
+     * @param c 指定类型  
+     * @return 如果不需要复制则返回真，否则返回假  
+     */    
+    private static boolean isNeedlessClone(Class c){    
+        if(c.isPrimitive()){//基本类型    
+            return true;    
+        }    
+        for(Class tmp:needlessCloneClasses){//是否在无需复制类型数组里    
+            if(c.equals(tmp)){    
+                return true;    
+            }    
+        }    
+        return false;    
+    }    
+        
+    /**  
+     * 尝试创建新对象  
+     * @param c 原始对象  
+     * @return 新的对象  
+     * @throws IllegalAccessException  
+     */    
+    private static Object createObject(Object value) throws IllegalAccessException{    
+            try {    
+                return value.getClass().newInstance();    
+            } catch (InstantiationException e) {    
+                return null;    
+            } catch (IllegalAccessException e) {    
+                throw e;    
+            }    
+    }    
+        
+    /**  
+     * 复制对象数据  
+     * @param value 原始对象  
+     * @param level 复制深度。小于0为无限深度，即将深入到最基本类型和Object类级别的数据复制；  
+     * 大于0则按照其值复制到指定深度的数据，等于0则直接返回对象本身而不进行任何复制行为。  
+     * @return 返回复制后的对象  
+     * @throws IllegalAccessException  
+     * @throws InstantiationException  
+     * @throws InvocationTargetException  
+     * @throws NoSuchMethodException  
+     */    
+    public static Object clone(Object value,int level) throws IllegalAccessException, InstantiationException, InvocationTargetException, NoSuchMethodException{    
+        if(value==null){    
+            return null;    
+        }    
+        if(level==0){    
+            return value;    
+        }    
+        Class c = value.getClass();    
+        if(isNeedlessClone(c)){    
+            return value;    
+        }    
+        level--;    
+        if(value instanceof Collection){//复制新的集合    
+            Collection tmp = (Collection)c.newInstance();    
+            for(Object v:(Collection)value){    
+                tmp.add(clone(v,level));//深度复制    
+            }    
+            value = tmp;    
+        }    
+        else if(c.isArray()){//复制新的Array    
+            //首先判断是否为基本数据类型    
+            if(c.equals(int[].class)){    
+                int[] old = (int[])value;    
+                value = (int[])Arrays.copyOf(old, old.length);    
+            }    
+            else if(c.equals(short[].class)){    
+                short[] old = (short[])value;    
+                value = (short[])Arrays.copyOf(old, old.length);    
+            }    
+            else if(c.equals(char[].class)){    
+                char[] old = (char[])value;    
+                value = (char[])Arrays.copyOf(old, old.length);    
+            }    
+            else if(c.equals(float[].class)){    
+                float[] old = (float[])value;    
+                value = (float[])Arrays.copyOf(old, old.length);    
+            }    
+            else if(c.equals(double[].class)){    
+                double[] old = (double[])value;    
+                value = (double[])Arrays.copyOf(old, old.length);    
+            }    
+            else if(c.equals(long[].class)){    
+                long[] old = (long[])value;    
+                value = (long[])Arrays.copyOf(old, old.length);    
+            }    
+            else if(c.equals(boolean[].class)){    
+                boolean[] old = (boolean[])value;    
+                value = (boolean[])Arrays.copyOf(old, old.length);    
+            }    
+            else if(c.equals(byte[].class)){    
+                byte[] old = (byte[])value;    
+                value = (byte[])Arrays.copyOf(old, old.length);    
+            }    
+            else {    
+                Object[] old = (Object[])value;    
+                Object[] tmp = (Object[])Arrays.copyOf(old, old.length, old.getClass());    
+                for(int i = 0;i<old.length;i++){    
+                    tmp[i] = clone(old[i],level);    
+                }    
+                value = tmp;    
+            }    
+        }    
+        else if(value instanceof Map){//复制新的MAP    
+            Map tmp = (Map)c.newInstance();    
+            Map org = (Map)value;    
+            for(Object key:org.keySet()){    
+                tmp.put(key, clone(org.get(key),level));//深度复制    
+            }    
+            value = tmp;    
+        }    
+        else {    
+            Object tmp = createObject(value);    
+            if(tmp==null){//无法创建新实例则返回对象本身，没有克隆    
+                return value;    
+            }    
+            Set<Field> fields = new HashSet<Field>();    
+            while(c!=null&&!c.equals(Object.class)){    
+                fields.addAll(Arrays.asList(c.getDeclaredFields()));    
+                c = c.getSuperclass();    
+            }    
+            for(Field field:fields){    
+                if(!Modifier.isFinal(field.getModifiers())){//仅复制非final字段    
+                    field.setAccessible(true);    
+                    field.set(tmp, clone(field.get(value),level));//深度复制    
+                }    
+            }    
+            value = tmp;    
+        }    
+        return value;    
+    }    
+        
+    /**  
+     * 浅表复制对象  
+     * @param value 原始对象  
+     * @return 复制后的对象，只复制一层  
+     * @throws IllegalAccessException  
+     * @throws InstantiationException  
+     * @throws InvocationTargetException  
+     * @throws NoSuchMethodException  
+     */    
+    public static Object clone(Object value) throws IllegalAccessException, InstantiationException, InvocationTargetException, NoSuchMethodException{    
+        return clone(value,1);    
+    }    
+        
+    /**  
+     * 深度复制对象  
+     * @param value 原始对象  
+     * @return 复制后的对象  
+     * @throws IllegalAccessException  
+     * @throws InstantiationException  
+     * @throws InvocationTargetException  
+     * @throws NoSuchMethodException  
+     */    
+    public static Object deepClone(Object value) throws IllegalAccessException, InstantiationException, InvocationTargetException, NoSuchMethodException{    
+        return clone(value,-1);    
+    } 
+    /**
+     * 将一个对象转换为另一个对象
+     * @param <T1> 要转换的对象
+     * @param <T2> 转换后的类
+     * @param orimodel 要转换的对象
+     * @param castClass 转换后的类
+     * @return 转换后的对象
+     */
+    public static  <T1,T2> T2 convertBean(T1 orimodel, Class<T2> castClass) {
+        T2 returnModel = null;
+        try {
+            returnModel = castClass.newInstance();
+        } catch (Exception e) {
+            throw new RuntimeException("创建"+castClass.getName()+"对象失败");
+        } 
+        List<Field> fieldlist = new ArrayList<Field>(); //要转换的字段集合
+        while (castClass != null && //循环获取要转换的字段,包括父类的字段
+                !castClass.getName().toLowerCase().equals("java.lang.object")) {
+            fieldlist.addAll(Arrays.asList(castClass.getDeclaredFields()));
+            castClass = (Class<T2>) castClass.getSuperclass(); //得到父类,然后赋给自己
+        }
+        for (Field field : fieldlist) {
+            PropertyDescriptor getpd = null;
+            PropertyDescriptor setpd = null;
+            try {
+                getpd= new PropertyDescriptor(field.getName(), orimodel.getClass());
+                setpd=new PropertyDescriptor(field.getName(), returnModel.getClass());
+            } catch (Exception e) {
+                continue;
+            }
+            try {
+                Method getMethod = getpd.getReadMethod();
+                Object transValue = getMethod.invoke(orimodel);
+                Method setMethod = setpd.getWriteMethod();
+                setMethod.invoke(returnModel, transValue);
+            } catch (Exception e) {
+                throw  new RuntimeException("cast "+orimodel.getClass().getName()+"to "
+                        +castClass.getName()+" failed");
+            }
+        }
+        return returnModel;
+    }
+}
