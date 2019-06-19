89942ebdff1eccfc6a806630d7bc2ee8e3dca491
zhaoxiaohu <zhaoxiaohu@yorma.net>
2019-06-19 15:27:08 +0800

    新增 邮件实体类

---
+++ src/main/java/com/yorma/common/utils/mail/SimpleMail.java
@@ -0,0 +1,49 @@
+package com.yorma.common.utils.mail;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ * @author zxh
+ */
+public class SimpleMail {
+	private List<String> recipients;
+	private String subject;
+	private String content;
+	private List<File> attachments;
+	
+	public List<String> getRecipients() {
+		return recipients;
+	}
+	
+	public void setRecipients(final List<String> recipients) {
+		if (recipients.stream().noneMatch(s -> s.matches(MailAuthenticator.REGEX))) {
+			throw new IllegalMailFormatException();
+		}
+		this.recipients = recipients;
+	}
+	
+	public String getSubject() {
+		return subject;
+	}
+	
+	public void setSubject(final String subject) {
+		this.subject = subject;
+	}
+	
+	public String getContent() {
+		return content;
+	}
+	
+	public void setContent(final String content) {
+		this.content = content;
+	}
+	
+	public List<File> getAttachments() {
+		return attachments;
+	}
+	
+	public void setAttachments(final List<File> attachments) {
+		this.attachments = attachments;
+	}
+}
